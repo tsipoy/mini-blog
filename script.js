d504dca75ab7755f720ab17ecf55e102d14ed689
Natacha <natacha.har@onja.org>
2020-07-04 16:55:06 +0300

    added function and added event listener

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 console.log('it works');
 
-// code your solution in here
+// code your solution in here 
+
+const createNewPost = () => {
+    let parentDiv = document.createElement('div');
+    parentDiv.setAttribute('id', 'post-list');
+
+    let newPost = document.createElement("div");
+    newPost.classList.add("card");
+    newPost.classList.add('some-space');
+
+    let div = document.createElement('div');
+    div.classList.add("card-body");
+
+    let heading = document.createElement('h5');
+    heading.classList.add("card-title");
+    heading.textContent = document.querySelector("#new-post-title").value;
+
+    let paragraph = document.createElement('p');
+    paragraph.classList.add("card-text");
+    paragraph.textContent = document.querySelector("#new-post-content").value;
+    
+    div.appendChild(heading);
+    div.appendChild(paragraph);
+    newPost.appendChild(div);
+    parentDiv.appendChild(newPost);
+    return parentDiv;
+}
+
+console.log(createNewPost());
+
+const submitButton = document.querySelector("#submit-form");
+submitButton.addEventListener('click', ($event) => {
+    let newBlogPost = document.querySelector('#post-list');
+    newBlogPost.appendChild(createNewPost());
+    $event.preventDefault();
+    commentForm.reset();
+});
+console.log(submitButton);
+
