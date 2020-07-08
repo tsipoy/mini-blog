05bc05448b340951f28a1c83cd953abf14496066
Natacha <natacha.har@onja.org>
2020-07-08 06:29:46 +0300

    Created the code with string

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('it works');
-
+/*
 // code your solution in here 
 
 const createNewPost = () => {
@@ -35,7 +35,49 @@ submitButton.addEventListener('click', ($event) => {
     let newBlogPost = document.querySelector('#post-list');
     newBlogPost.appendChild(createNewPost());
     $event.preventDefault();
-    commentForm.reset();
 });
 console.log(submitButton);
+*/
+
+// grab the elements we need
+const postList = document.querySelector('#post-list');
+const submitBtn = document.querySelector('#submit-form');
+const postTitleInput = document.querySelector('#new-post-title');
+const postContentTextarea = document.querySelector('#new-post-content');
+const firstCheckBox = document.querySelector('#hobbies-tag');
+const secondCheckBox = document.querySelector('#school-life-tag');
+const thirdCheckBox = document.querySelector('#personal-tag');
+const addPostForm = document.querySelector('form');
+
+// add the event listener on the submit, click
+submitBtn.addEventListener('click', ($event) => {
+	$event.preventDefault();
+
+	// ************** CREATE HTML ELEMENT ****************
+
+	const myHTMLPost = `
+    <div class="card some-space">
+        <div class="card-body">
+            <h5 class="card-title">${postTitleInput.value}</h5>
+            <p class="card-text">${postContentTextarea.value}</p>
+            <span class="badge badge-light ${!firstCheckBox.checked ? 'hidden' : ''}">Hobbies</span>
+            <span class="badge badge-light ${
+				!secondCheckBox.checked ? 'hidden' : ''
+			}">School life</span>
+            <span class="badge badge-light ${
+				!thirdCheckBox.checked ? 'hidden' : ''
+			}">Personal</span>
+        </div>
+    </div>
+    `;
+
+	// another way to create the HTML element inside javascript
+	//const myFragment = document.createRange().createContextualFragment(myHTMLPost);
+	//postList.appendChild(myFragment)
+
+	postList.innerHTML += myHTMLPost;
+
+	addPostForm.reset();
+});
+
 
